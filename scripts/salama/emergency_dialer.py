c2b7f30ceee43b418fb587ae53267f933427bcd3
Euan Millar <euanmillar@Euans-MacBook-Pro.local>
2017-12-21 05:46:04 +0000

    python logging

--- scripts/salama/emergency_dialer.py
+++ scripts/salama/emergency_dialer.py
@@ -7,6 +7,8 @@ from optparse import OptionParser
 import sys
 from xml.etree import ElementTree
 import ESL
+import logging
+logging.basicConfig(filename='caller-log.log',level=logging.DEBUG)
 
 def find_between( s, first, last ):
     try:
@@ -23,7 +25,7 @@ def main(argv):
                       help='ESL password')
     parser.add_option('-s', '--server', dest='server', default='127.0.0.1',
                       help='FreeSWITCH server IP address')
-    parser.add_option('-p', '--port', dest='port', default='8021',
+    parser.add_option('-p', '--port', dest='port', default='5062',
                       help='FreeSWITCH server event socket port')
     parser.add_option('-c', '--command', dest='command', default='status',
                       help='command to run, surround multi-word commands in ""s')
@@ -33,16 +35,22 @@ def main(argv):
     con = ESL.ESLconnection(options.server, options.port, options.auth)
  
     if not con.connected():
+        logging.debug('Not Connected')
         sys.exit(2)
 
     e = con.api(options.command)
     if e:
+        logging.debug(e.getBody())
         userID = sys.argv[1]
         toBeContacted = sys.argv[2]
         userExtension = sys.argv[3]
+        logging.debug(userID)
+        logging.debug(toBeContacted)
+        logging.debug(userExtension)
         emergencyContacts = toBeContacted.split(',')
         for user in emergencyContacts:
             dialString = str('originate {contact=' + user + ',userExtension=' + userExtension + ',userID=' + userID + '}sofia/internal/' + user + '@127.0.0.1:5062 113')
+            logging.debug(dialString)
             con.api(dialString)
  
  
